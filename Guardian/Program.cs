15be4bfcb64df74fa1d8b69833c6b432752e6c3b
Zhibin Wang <w-zb@outlook.com>
2015-07-21 23:08:08 +0800

    version 1.0 finished

--- Guardian/Program.cs
+++ Guardian/Program.cs
@@ -12,20 +12,69 @@ namespace Guardian
     {
         static void Main(String[] args)
         {
-            //Memo memo = new Memo 
-            //{
-            //    Company = "阿里",
-            //    Email = "w-zb@outlook.com",
-            //    Password = "WZB@Alipay",
-            //    Product = "支付宝",
-            //    UserName = "w-zb@outlook.com"
-            //};
+            while (true)
+            {
+                Console.Write("INPUT: ");
+                string input = Console.ReadLine();
 
-            //memo.Save();
+                Console.WriteLine("OUTPUT: E-{0}; D-{1}", Encrypt(input), Decrypt(input));
+            }
+        }
 
-            Memo _memo = new Memo(1);
+        static string Encrypt(string input)
+        {
+            // 奇数位置字符的ASKII位序加4，偶数位加1后进行Base64编码
 
-            File.WriteAllText("output.txt", _memo.Password);
+            int index = 0;
+            string output = string.Empty;
+
+            foreach (char i in input)
+            {
+                if (index % 2 == 0)
+                {
+                    output += (char)(i + 1);
+                }
+                else
+                {
+                    output += (char)(i + 4);
+                }
+
+                index++;
+            }
+
+            return Convert.ToBase64String(Encoding.ASCII.GetBytes(output));
+        }
+
+        static string Decrypt(string input)
+        {
+            // Base64解码后奇数位字符ASKII位序各减4，偶数位减1
+
+            try
+            {
+                int index = 0;
+                string output = null;
+
+                foreach (byte i in Convert.FromBase64String(input))
+                {
+                    if (index % 2 == 0)
+                    {
+                        output += (char)(i - 1);
+                    }
+                    else
+                    {
+                        output += (char)(i - 4);
+                    }
+
+                    index++;
+                }
+
+                return output;
+            }
+            catch(Exception ecp)
+            {
+                Console.WriteLine(ecp.Message);
+                return string.Empty;
+            }
         }
     }
 }
